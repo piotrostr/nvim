c2cf4b96f1690c54524e22dddbc101500fcb6ad9
piotrostr <piotr.ostrowski@aol.com>
2022-09-04 00:17:26 +0200

    feat: add some indent options (yet to make automatic [still new to lua])

--- init.lua
+++ init.lua
@@ -4,3 +4,20 @@ require('piotrostr.maps')
 require('piotrostr.plugins')
 
 vim.cmd([[colorscheme gruvbox]])
+
+
+local set = vim.opt
+
+set.tabstop = 2
+set.autoindent = true
+set.smarttab = true
+set.expandtab = true
+set.cursorline = false
+
+-- go
+set.tabstop = 8
+set.shiftwidth = 8
+
+-- tsx
+set.tabstop = 2
+set.shiftwidth = 2
