19abaf2f08cde56c2197446c44ddfc12721db5aa
Piotr Ostrowski <piotrostr@google.com>
2022-09-05 12:25:03 +0200

    feat: add c-based fzf ext for blazingly fast telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,12 +7,14 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
+  use 'wbthomason/packer.nvim' -- pacman
+  use { "ellisonleao/gruvbox.nvim" } -- colorscheme
   use 'ray-x/go.nvim'
-  use 'ray-x/guihua.lua' -- recommanded if need floating window support
-  use { "ellisonleao/gruvbox.nvim" }
-  use 'wbthomason/packer.nvim'
+  use 'L3MON4D3/LuaSnip' -- dep
+  use 'ray-x/guihua.lua' -- dep
+  use 'dinhhuy258/git.nvim' -- dep
+  use 'nvim-lua/plenary.nvim' -- dep
   use 'nvim-lualine/lualine.nvim' -- Statusline
-  use 'nvim-lua/plenary.nvim' -- Common utilities
   use 'onsails/lspkind-nvim' -- vscode-like pictograms
   use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
   use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
@@ -22,7 +24,6 @@ packer.startup(function(use)
   use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
-
   use 'glepnir/lspsaga.nvim' -- LSP UIs
   use {
     'nvim-treesitter/nvim-treesitter',
@@ -31,6 +32,10 @@ packer.startup(function(use)
   use 'kyazdani42/nvim-web-devicons' -- File icons
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make'
+  }
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
   use 'norcalli/nvim-colorizer.lua'
@@ -41,6 +46,4 @@ packer.startup(function(use)
   use 'akinsho/nvim-bufferline.lua'
   use 'aserowy/tmux.nvim'
   use 'lewis6991/gitsigns.nvim'
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
-  use 'L3MON4D3/LuaSnip'
 end)
