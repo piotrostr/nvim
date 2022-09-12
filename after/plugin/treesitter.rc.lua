f93d83d2c73f61534725a1e7008700f05c4a499c
piotrostr <piotr.ostrowski@aol.com>
2022-09-12 10:49:30 +0100

    fix: dont require any treesitter parsers (to go easy on m1 mac)

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -11,15 +11,15 @@ ts.setup {
     disable = {},
   },
   ensure_installed = {
-    "tsx",
-    "solidity",
-    "toml",
-    "json",
-    "yaml",
-    "css",
-    "lua",
-    "go",
-    "python"
+    --     "tsx",
+    --     "solidity",
+    --     "toml",
+    --     "json",
+    --     "yaml",
+    --     "css",
+    --     "lua",
+    --     "go",
+    --     "python"
   },
   autotag = {
     enable = true,
@@ -29,7 +29,7 @@ ts.setup {
 -- uncomment below on macos if cannot compile
 -- sometimes might need to :TSInstall <parser> manually as well for it to work
 --
--- require 'nvim-treesitter.install'.compilers = { 'aarch64-apple-darwin21-gcc-11' }
+require 'nvim-treesitter.install'.compilers = { 'aarch64-apple-darwin21-gcc-11' }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
