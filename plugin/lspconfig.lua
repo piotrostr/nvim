792bd0a67326e8c7cc468990f9efdf7469b319b1
Piotr Ostrowski <piotrostr@google.com>
2023-02-14 18:07:27 +0000

    fix 'E790: undo join is not allowed after undo'

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -105,7 +105,7 @@ nvim_lsp.sumneko_lua.setup {
 vim.cmd [[
   augroup fmt
     autocmd!
-    autocmd BufWritePre * undojoin | Neoformat
+    au BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
   augroup END
 ]]
 
