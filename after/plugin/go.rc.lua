ee25c19a745f7c0f07f47812cdaa6befc78587f7
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-10-05 13:54:24 +0200

    fix go autoindent

--- after/plugin/go.rc.lua
+++ after/plugin/go.rc.lua
@@ -4,4 +4,19 @@ require('go').setup()
 vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
 
 -- set go ident and tab size
-vim.api.nvim_exec([[ autocmd FileType go setlocal shiftwidth=2 tabstop=2 ]], false)
+vim.api.nvim_exec([[ autocmd FileType go setlocal shiftwidth=2 tabstop=2 autoindent smartindent cindent ]], false)
+
+vim.api.nvim_create_autocmd("FileType", {
+ pattern = "go",
+ callback = function()
+   vim.bo.indentexpr = ""
+ end
+})
+
+require'nvim-treesitter.configs'.setup {
+ indent = {
+   enable = true,
+   disable = { "go" }  -- add this line to disable for Go
+ },
+}
+
