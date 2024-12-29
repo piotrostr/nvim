078ed1018f148076857e98fd43f981489d7573f8
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-12-29 15:21:39 +0100

    drop icons

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -7,7 +7,7 @@ bufferline.setup({
             icon = '',
             style = 'none'
         },
-        show_buffer_icons = true,
+        show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
         show_tab_indicators = false,
